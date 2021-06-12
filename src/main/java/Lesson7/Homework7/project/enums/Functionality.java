3fe4bbd86ad003663492add6529d41d213e3ef01
paytkovsg <paytkov.s@gmail.com>
2021-06-12 14:27:44 +0500

    дз7 .

---
+++ src/main/java/Lesson7/Homework7/project/enums/Functionality.java
@@ -0,0 +1,6 @@
+package Lesson7.Homework7.project.enums;
+
+public enum Functionality {
+    GET_CURRENT_WEATHER,
+    GET_WEATHER_IN_NEXT_5_DAYS
+}
---
+++ src/main/java/Lesson7/Homework7/project/enums/Periods.java
@@ -0,0 +1,7 @@
+package Lesson7.Homework7.project.enums;
+
+public enum Periods {
+    NOW,
+    FIVE_DAYS,
+    CUSTOM
+}
